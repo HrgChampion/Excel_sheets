1edba4a6b574b13a825648990e787ac60cebf025
Himanshu Gauba <hgauba4@gmail.com>
2024-12-28 00:14:36 +0530

    formula cell application, graph cycle detection

--- grid.js
+++ grid.js
@@ -26,6 +26,9 @@ for(let i = 0; i < rows; i++){
     for(let j = 0; j < cols; j++){
         let cell = document.createElement("div");
         cell.setAttribute("class","cell");
+        cell.setAttribute("rid",i);
+        cell.setAttribute("cid",j);
+        cell.setAttribute("spellcheck","false");
         cell.setAttribute("contenteditable","true");
         rowCont.appendChild(cell);
         addEventListenerForAddressBarDisplay(cell,i,j);
@@ -40,3 +43,7 @@ let colId = String.fromCharCode(j + 65);
 addressBar.value = `${colId}${rowId}`
 })
 }
+
+// Default click on first cell
+let firstCell = document.querySelector(".cell");
+firstCell.click();
